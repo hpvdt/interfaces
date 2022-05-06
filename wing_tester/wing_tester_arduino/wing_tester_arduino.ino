084c38c14b5db6af4d18f3eb0e117305d32f5c71
Savo <56771837+savob@users.noreply.github.com>
2022-05-06 14:20:06 -0400

    Fixed the order of gain setting
    Need to set the gain for the future reading to be taken, not the one that immediately follows the command since setting gain does NOT update the previously prepared value to be read.

--- wing_tester/wing_tester_arduino/wing_tester_arduino.ino
+++ wing_tester/wing_tester_arduino/wing_tester_arduino.ino
@@ -48,17 +48,34 @@ void loop() {
   Serial.print(0);
   i++;
 #else
-  // Normal operation
-  // Reads all possible values for the system
+  /* Normal operation
+    Reads all possible values for the system.
+
+    NOTE: Due to the operation of the HX711 chips the gain setting is not 
+    as expected (e.g. setting it to 32 before reading for channel B). This 
+    is due to us waiting for data being ready before setting the gain and 
+    then reading.
+
+    The data readied for us to read is from the previous gain setting since 
+    the gain change has not had the time to come into effect on the data 
+    prepared. So what we are effectively doing is setting the gain for the 
+    next reading not the one that immediately follows a gain change.
+
+    ALSO NOTE: For a given HX711 channel A is the even numbered header, 
+    channel B is used for the odd numbered header.
+  */
+  
   if (scale12.is_ready()) {
-    // with a gain factor of 32, channel B is selected
-    scale12.set_gain(32);
+    // With a gain factor of 64 or 128, channel A is selected for the next read
+    scale12.set_gain(128); 
     long reading1 = scale12.read();
     Serial.print(reading1);
     Serial.print(separator);
 
-    // with a gain factor of 64 or 128, channel A is selected
-    scale12.set_gain(128);
+    scale12.wait_ready(1); // Wait in 1ms increments until ready to read
+
+    // With a gain factor of 32, channel B is selected for the next read
+    scale12.set_gain(32); 
     long reading2 = scale12.read();
     Serial.print(reading2);
     Serial.print(separator);
@@ -68,14 +85,16 @@ void loop() {
   }
 
   if (scale34.is_ready()) {
-    // with a gain factor of 32, channel B is selected
-    scale34.set_gain(32);
+    // With a gain factor of 64 or 128, channel A is selected for the next read
+    scale34.set_gain(128);
     long reading3 = scale34.read();
     Serial.print(reading3);
     Serial.print(separator);
 
-    // with a gain factor of 64 or 128, channel A is selected
-    scale34.set_gain(128);
+    scale34.wait_ready(1); // Wait in 1ms increments until ready to read
+
+    // With a gain factor of 32, channel B is selected for the next read
+    scale34.set_gain(32);
     long reading4 = scale34.read();
     Serial.print(reading4);
     Serial.print(separator);
@@ -85,14 +104,16 @@ void loop() {
   }
 
   if (scale56.is_ready()) {
-    // with a gain factor of 32, channel B is selected
-    scale56.set_gain(32);
+    // With a gain factor of 64 or 128, channel A is selected for the next read
+    scale56.set_gain(128);
     long reading5 = scale56.read();
     Serial.print(reading5);
     Serial.print(separator);
 
-    // with a gain factor of 64 or 128, channel A is selected
-    scale56.set_gain(128);
+    scale56.wait_ready(1); // Wait in 1ms increments until ready to read
+
+    // With a gain factor of 32, channel B is selected for the next read
+    scale56.set_gain(32);
     long reading6 = scale56.read();
     Serial.print(reading6);
   } else {
